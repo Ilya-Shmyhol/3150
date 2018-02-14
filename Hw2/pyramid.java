1404e4fe0ebcfa8e3b793131973dd7b6108aa5c0
Ilya-Shmyhol <shmyhol@gmail.com>
2018-02-14 15:18:16 -0500

    Update pyramid.java

--- Hw2/pyramid.java
+++ Hw2/pyramid.java
@@ -10,18 +10,26 @@ public class pyramid {
 			c--;
 			linenum++;
 			for(int j=0;j<c;j++){//print spaces
-				System.out.print(" ");
+				System.out.print("   ");
+				//
 			}//print the digits
 			//using line num we need to print every value less than line num 
 			//starting at one, then line num then every number less than line num ending at zero
 			//line num and 
 			//System.out.print(c);
 			for(int b=1;b<linenum;b++){
+				if(b<10)System.out.print("  ");
+				else System.out.print(" ");
 				System.out.print(b);
 			}
+			
+				System.out.print(" ");
 			//System.out.print(linenum);
 			for(int d=linenum;d>0;d--){
+				if(d<10)System.out.print("  ");
+				else System.out.print(" ");
 				System.out.print(d);
+				System.out.print(" ");
 			}
 			System.out.print("\n");
 		}
